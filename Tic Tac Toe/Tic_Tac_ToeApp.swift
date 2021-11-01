e0c6d8f0853947b7d3aa11e0c136d57d33a2bc6e
Ryan Lim <rlim@bsd220.org>
2021-11-01 10:55:11 -0500

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Student on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
